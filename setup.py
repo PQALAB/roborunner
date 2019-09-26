86d2116d2771a51973ec6b11439ac4532fd12777
nick-grout <nick@plusqa.com>
2019-09-26 11:43:55 -0700

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 
 setup(
     name             = 'robotframework-roborunner',
